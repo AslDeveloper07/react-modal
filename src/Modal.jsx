6c7514bb6f01490e41363b415a865d7cb706acc6
Asilbek Suvonov <aslwexking777@gmail.com>
2025-08-05 00:43:21 +0500

    hg

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -4,7 +4,7 @@ import { IoMdClose } from "react-icons/io";
 const Modal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
   return (
-    <div onClick={onClose} className="fixed h-full w-full bg-[#00000027] backdrop-blur-3xl flex items-center justify-center z-44 px-4">
+    <div onClick={onClose} className="fixed h-full w-full bg-[#0000003c] backdrop-blur-3xl flex items-center justify-center z-44 px-4">
       <div className="  bg-white rounded-xl shadow-2xl max-w-md w-full overflow-hidden relative animate-fade-in-up">
         <img src="https://i.pinimg.com/736x/16/27/c5/1627c5a6c344322634d864565b4cb67b.jpg" alt="" className="w-full h-[370px] object-cover" />
         <div className=" p-4 ">
@@ -17,7 +17,7 @@ const Modal = ({ isOpen, onClose }) => {
 
           <button
             onClick={onClose}
-            className="bg-blue-600 text-white text-xl w-full hover:bg-blue-500 transition duration-200 mt-10 py-2 px-4 rounded-md"
+            className="bg-orange-600 cursor-pointer text-white text-xl w-full hover:bg-orange-700 transition duration-200 mt-10 py-2 px-4 rounded-md"
           >
             Yopish
           </button>
